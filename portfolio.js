1fc0e2cba9ddf0f82e47e303168d6eb28bee55e6
AnkitPandey221095 <ankitpandey221095@gmail.com>
2025-02-08 21:09:35 +0530

    adding margin

--- portfolio.js
+++ portfolio.js
@@ -1,5 +1,5 @@
 var typed = new Typed('.text', {
-    strings: ["Front End developer", "Web Developer"],
+    strings: ["Data Analyst", "Web Developer"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
